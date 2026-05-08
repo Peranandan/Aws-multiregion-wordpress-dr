f464fcc0093dd5a0de8e694360024e4a0ac707c9
Peranandan <peranandan.m@gmail.com>
2026-05-08 21:07:13 +0530

    Create Wordpress-setup.sh

---
+++ Scripts/Wordpress-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+dnf update -y
+dnf install -y httpd php php-mysqlnd wget tar
+cd /var/www/html
+echo "healthy" > healthy.html
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+cp -r wordpress/* /var/www/html/
+rm -rf wordpress
+rm -rf latest.tar.gz
+chmod -R 755 wp-content
+chown -R apache:apache wp-content
+wget https://s3.amazonaws.com/bucketforwordpresslab-donotdelete/htaccess.txt
+mv htaccess.txt .htaccess
+systemctl enable httpd
+systemctl start httpd
